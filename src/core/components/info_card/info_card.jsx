1703bb8e829a3d2d9f1d686bbb2d78d061554ce4
Kripa Sindhu Bairagi <kripa@wooshair.com>
2024-02-08 16:53:22 +0530

    Added: Responsibilities

--- src/core/components/info_card/info_card.jsx
+++ src/core/components/info_card/info_card.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./info_card.module.css";
 
-const InfoCard = ({ image, link, details, title }) => {
+const InfoCard = ({ image, link, details, title, contributions }) => {
     return (
         <div className={styles.card}>
             <h2>{title}</h2>
             <p>{details}</p>
+            {contributions?.map((e) => <p className={styles.contribution}>{e}</p>)}
             <Link to={link}>
                 <img src={image} alt={title} className={styles.image} />
             </Link>
--- src/features/home/components/works/works.jsx
+++ src/features/home/components/works/works.jsx
@@ -23,24 +23,51 @@ const Works = () => {
                 title={"Woosh Air"}
                 link={"https://wooshair.com/"}
                 details={"WooshAir specializes in the development of smart filters and air quality monitors that seamlessly communicate with mobile devices, providing real-time telemetry data."}
+                contributions={[
+                    "- Contributed to the development of an intelligent air filtration system coupled with an air quality monitoring solution.",
+                    "- Architected and implemented the mobile application, facilitating user onboarding processes and providing real-time device telemetry insights.",
+                    "- Engineered an onboarding workflow for establishing seamless integration between the mobile application and the smart module/air quality monitor.",
+                    "- Worked on pipelines for building and deploying application.",
+                    "- Implemented deep links and notifications on the application.",
+                    "- Flutter, BLoC, Firebase, ReactJS, NodeJS"
+                ]}
             />
             <InfoCard
                 image={beyondexams}
                 title={"BeyondExams"}
                 link={"https://beyondexams.org/"}
                 details={"BeyodExams is an educational platform that facilitates both instructional and learning experiences through video content, providing users with the capability to engage in teaching or learning activities."}
+                contributions={[
+                    "- Beyondexams is an educational platform that facilitates both instructional and learning experiences through video content, providing users with the capability to engage in teaching or learning activities",
+                    "- Engaged in the design and implementation of an aesthetically pleasing and functionally robust user interface.",
+                    "- Implemented and tested the API integration.",
+                    "- Created a responsive User Interface for different window sizes.",
+                    "- Technologies/Frameworks: ReactJS, NodeJS"
+                ]}
             />
             <InfoCard
                 image={techweirdo}
                 title={"TechWeirdo"}
                 link={"https://techweirdo.in/"}
                 details={"Techweirod operates as a service-based company, delivering an extensive array of specialized services tailored to meet the diverse needs of our clients. With a commitment to excellence and innovation, we strive to provide comprehensive solutions that exceed expectations and drive success in today's dynamic business landscape."}
+                contributions={[
+                    "- Conceived and implemented client-requested applications, assuming end-to-end project ownership and actively engaging in client interactions throughout the development lifecycle.",
+                    "- Created algorithms to parse Excel file and extract data in order.",
+                    "- Created mobile application, web application and backend application.",
+                    "- Managed and trained interns on various projects with task allocation using Waterfall Model.",
+                    "- Technologies/Frameworks: Flutter, NodeJS(Express), ReactJS, MongoDB, REST APIs"
+                ]}
             />
             <InfoCard
                 image={stickman}
                 title={"Stickman"}
                 link={"https://www.stickmanservices.com/"}
                 details={"Stickman services offers a range of services including Marketing, Digital Marketing, Trade Marketing, Branding, Growth Hacking, Technology Solutions, App Development, Website Development, Boutique Software Development, Website Design, Mobile App Marketing, Marketing Research, SEO, SEM, SMO, Event Management, and Product Launch."}
+                contributions={[
+                    "- Designed and developed web application for e-commerce site for users, sellers and admin panel.",
+                    "- Designed and developed a quiz platform.",
+                    "- Technologies/Frameworks: ReactJS, MongoDB, REST APIs"
+                ]}
             />
 
         </div>
