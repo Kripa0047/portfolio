911fe083bb6132da6a2e2d13ef4bf70e73131d12
Kripa Sindhu Bairagi <kripa@wooshair.com>
2024-02-08 15:57:20 +0530

    Added: Tab title and icon

--- src/features/home/home.jsx
+++ src/features/home/home.jsx
@@ -2,10 +2,14 @@ import React from "react";
 // import styles from "./home.module.css";
 import { StartDetails, Works, About, Constants } from "./components";
 import { Header } from "../../core/components";
+import { Helmet } from 'react-helmet';
 
 const HomePage = () => {
     return (
         <div>
+            <Helmet>
+                <title> kripa.dev</title>
+            </Helmet>
             <Header />
             <StartDetails />
             <Works />
